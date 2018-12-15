ebd7cc2fe150e483dd2c8bfff5b110a735941590
wowo2107 <963287035@qq.com>
2018-12-16 01:02:21 +0900

    restart

---
+++ py/11-21.py
@@ -0,0 +1,3 @@
+#试一试无敌的调用吧
+from 12-15 import max
+print(max(2,3))
---
+++ py/12-15.py
@@ -0,0 +1,18 @@
+'''
+print(abs(-100))
+abs('a')
+print(int('124'))
+'''
+# -*- coding: utf-8 -*-
+#n1 = 255
+#n2 = 1000
+#print(hex(n1))
+#print(hex(n2))
+
+def max(a,b):
+    if a >= b:
+        return a
+    else:
+        return b
+
+print(max('abd','abc'))
